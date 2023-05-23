9fd1f7ddf8182e51864b6c20886eca8e1e325b0d
shiluvelomakhubele <117033636+ShiluveloMakhubel@users.noreply.github.com>
2023-05-23 14:08:09 +0200

    home page update

--- Capstone MeetApp/libs/app/home/feature/src/lib/homepage/homepage.component.ts
+++ Capstone MeetApp/libs/app/home/feature/src/lib/homepage/homepage.component.ts
@@ -8,4 +8,42 @@ import { CommonModule } from '@angular/common';
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css'],
 })
-export class HomepageComponent {}
+export class HomepageComponent {
+  data = [
+    {
+      imageSrc: 'path/to/your/image1.jpg',
+      title: 'Joburg Art Fair 1',
+      location: 'Sandton',
+      date: '8-10 September 2023',
+      description: 'The annual Joburg Art Fair will showcase the best in contemporary African art and design, with 23 galleries and 11'
+    },
+    {
+      imageSrc: 'path/to/your/image1.jpg',
+      title: 'Joburg Art Fair 1',
+      location: 'Sandton',
+      date: '8-10 September 2023',
+      description: 'The annual Joburg Art Fair will showcase the best in contemporary African art and design, with 23 galleries and 11'
+    },
+    {
+      imageSrc: 'path/to/your/image1.jpg',
+      title: 'Joburg Art Fair 1',
+      location: 'Sandton',
+      date: '8-10 September 2023',
+      description: 'The annual Joburg Art Fair will showcase the best in contemporary African art and design, with 23 galleries and 11'
+    },
+    {
+      imageSrc: 'path/to/your/image1.jpg',
+      title: 'Joburg Art Fair 1',
+      location: 'Sandton',
+      date: '8-10 September 2023',
+      description: 'The annual Joburg Art Fair will showcase the best in contemporary African art and design, with 23 galleries and 11'
+    }
+    
+  ];
+}
+
+
+
+
+
+
