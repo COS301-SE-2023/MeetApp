f08537867012a7f222239581a35e3c090d9417bb
shiluvelomakhubele <117033636+ShiluveloMakhubel@users.noreply.github.com>
2023-05-26 19:52:00 +0200

    homepage update

--- Capstone MeetApp/libs/app/home/feature/src/lib/homepage/homepage.component.ts
+++ Capstone MeetApp/libs/app/home/feature/src/lib/homepage/homepage.component.ts
@@ -1,43 +1,49 @@
-
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms'
-import { FormBuilder,  Validators } from '@angular/forms';
-import { Router } from "@angular/router";
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormGroup, FormControl } from '@angular/forms';
-import {ApiService } from '../../../../../shared service/api.service';
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-
-
-// eslint-disable-next-line @nx/enforce-module-boundaries
-import { IEvent } from 'libs/app/utils/interfaces/event.interface';
-
 
 @Component({
   selector: 'capstone-meet-app-homepage',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule],
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css'],
-  providers: [ApiService]
 })
 export class HomepageComponent {
-
-  results: IEvent[];
-  constructor(private router: Router, private apiService: ApiService){
-     
-    this.results = this.apiService.getAllEventsMock();
-  }
-  
-  
-  
+  data = [
+    {
+      imageSrc: 'path/to/your/image1.jpg',
+      title: 'Joburg Art Fair 1',
+      location: 'Sandton',
+      date: '8-10 September 2023',
+      description: 'The annual Joburg Art Fair will showcase the best in contemporary African art and design, with 23 galleries and 11'
+    },
+    {
+      imageSrc: 'path/to/your/image1.jpg',
+      title: 'Joburg Art Fair 1',
+      location: 'Sandton',
+      date: '8-10 September 2023',
+      description: 'The annual Joburg Art Fair will showcase the best in contemporary African art and design, with 23 galleries and 11'
+    },
+    {
+      imageSrc: 'path/to/your/image1.jpg',
+      title: 'Joburg Art Fair 1',
+      location: 'Sandton',
+      date: '8-10 September 2023',
+      description: 'The annual Joburg Art Fair will showcase the best in contemporary African art and design, with 23 galleries and 11'
+    },
+    {
+      imageSrc: 'path/to/your/image1.jpg',
+      title: 'Joburg Art Fair 1',
+      location: 'Sandton',
+      date: '8-10 September 2023',
+      description: 'The annual Joburg Art Fair will showcase the best in contemporary African art and design, with 23 galleries and 11'
+    }
     
-  
+  ];
 }
-  
+
+
+
 
 
 
