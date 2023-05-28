3424791c4b4c0bb22fd4315847521293668b9c95
LesediMoko <lesedimokonyama@gmail.com>
2023-05-28 21:08:02 +0200

    event.schema.ts props added

--- Capstone MeetApp/libs/api/backend/src/events/schema/event.schema.ts
+++ Capstone MeetApp/libs/api/backend/src/events/schema/event.schema.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import { Prop, Schema, SchemaFactory, raw } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
 import { Category } from 'src/utils/enums';
@@ -8,7 +7,38 @@ export type EventDocument = HydratedDocument<Event>;
 
 @Schema({collection : "Events"})
 export class Event {
-
+    @Prop()
+    ID: mongoose.Schema.Types.ObjectId;
+  
+    @Prop()
+    name: string;
+  
+    @Prop()
+    organisation: string;
+  
+    @Prop()
+    description: string;
+  
+    @Prop()
+    date: string;
+  
+    @Prop()
+    startTime: string;
+  
+    @Prop()
+    endTime: string;
+  
+    @Prop(raw({
+      lng: { type: Number },
+      lat: { type: Number }
+    }))
+    location: Record<number,any>;
+  
+    @Prop()
+    category: Category;
+  
+    @Prop()
+    region: string;
   
 }
 
