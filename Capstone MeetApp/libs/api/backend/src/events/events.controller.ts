1d57f40cf2d0d5a5033657960282ec257f45b660
LesediMoko <lesedimokonyama@gmail.com>
2023-05-28 20:59:58 +0200

    eventsController findAll() findOne() and findbyOrg

--- Capstone MeetApp/libs/api/backend/src/events/events.controller.ts
+++ Capstone MeetApp/libs/api/backend/src/events/events.controller.ts
@@ -1,12 +1,5 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
@@ -27,8 +20,44 @@ export class EventsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.eventsService.findOne(+id);
+    return this.eventsService.findOne(id);
   }
+
+  @Get(':id/attendees')
+
+  @Get(':id/organiser')
+
+  @Get('today')
+
+  @Get('thisweek')
+
+  @Get('thismonth')
+
+  @Get('thisyear')
+
+  @Get(':region')
+
+  @Get('month/:month')
+
+  @Get('date/:date')
+
+  @Get('year/:year')
+
+  @Get('category/:category')
+
+  @Get('duration/:duration')
+
+  @Get('timeofday/:timeofday')
+
+  @Get('org/:organisation')
+  findbyOrganisation(@Param('organisation') organisation: string) {
+    //const parsedOrg = organisation.replace('%20',' ');
+    //console.log(parsedOrg);
+    //console.log(organisation);
+    //console.log(3);
+    return this.eventsService.findbyOrganisation(organisation);
+  }
+
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateEventDto: UpdateEventDto) {
