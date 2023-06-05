ef05f064ab054e19849129d1016581b36723551b
LesediMoko <lesedimokonyama@gmail.com>
2023-06-05 22:43:06 +0200

    users controller api

--- Capstone MeetApp/libs/api/backend/src/users/users.controller.ts
+++ Capstone MeetApp/libs/api/backend/src/users/users.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -27,7 +19,7 @@ export class UsersController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+    return this.usersService.findOne(id);
   }
 
   @Patch(':id')
