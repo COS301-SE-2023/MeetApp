9531d974ffe26c5392c7ab4ebcb84854376f0498
LesediMoko <lesedimokonyama@gmail.com>
2023-06-05 22:47:50 +0200

    import of missing modules

--- Capstone MeetApp/libs/api/backend/src/lib/backend.module.ts
+++ Capstone MeetApp/libs/api/backend/src/lib/backend.module.ts
@@ -6,7 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 
 @Module({
-  imports: [EventsModule, UsersModule, OrganisationsModule, MongooseModule.forRoot('mongodb+srv://u19007443:AGGGyM0C7n4VyBtN@cluster0.nh0ftux.mongodb.net/?retryWrites=true&w=majority', {dbName: 'MeetAppMockDB'})],
+  imports: [UsersModule, EventsModule, UsersModule, OrganisationsModule, MongooseModule.forRoot('mongodb+srv://u19007443:AGGGyM0C7n4VyBtN@cluster0.nh0ftux.mongodb.net/?retryWrites=true&w=majority', {dbName: 'MeetAppMockDB'})],
   controllers: [],
   providers: [],
 })
