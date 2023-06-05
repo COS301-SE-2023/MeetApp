c6d5e38e391760b0f740632be3225252cc3402ac
LesediMoko <lesedimokonyama@gmail.com>
2023-06-05 22:38:16 +0200

    users.module api

--- Capstone MeetApp/libs/api/backend/src/users/users.module.ts
+++ Capstone MeetApp/libs/api/backend/src/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService]
 })
 export class UsersModule {}
