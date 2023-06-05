863514bfa16d3b802da096baff23a97c06bc9b5f
LesediMoko <lesedimokonyama@gmail.com>
2023-06-05 22:42:10 +0200

    users service api

--- Capstone MeetApp/libs/api/backend/src/users/users.service.ts
+++ Capstone MeetApp/libs/api/backend/src/users/users.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>){
+    
+  }
+  
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.userModel.find().exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    return this.userModel.findById(id).exec();
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
