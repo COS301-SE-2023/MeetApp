17f08e89d32acc7a990412697ece02fabf5a6894
AkaniHlungwani <u19240202@tuks.co.za>
2023-06-06 22:57:48 +0200

    Added create-event dto for events

--- Capstone MeetApp/libs/api/backend/src/events/dto/create-event.dto.ts
+++ Capstone MeetApp/libs/api/backend/src/events/dto/create-event.dto.ts
@@ -1,1 +1,54 @@
-export class CreateEventDto {}
+import { IsNotEmpty, IsString, MaxLength} from 'class-validator';
+export class CreateEventDto {
+   
+    @IsNotEmpty()
+    readonly ID: mongoose.Schema.Types.ObjectId;
+    
+    @IsString()
+    @MaxLength(30)
+    @IsNotEmpty()
+    readonly name: string;
+    
+    @IsString()
+    @MaxLength(30)
+    @IsNotEmpty()
+    readonly organisation: string;
+    
+    @IsString()
+    @MaxLength(250)
+    @IsNotEmpty()
+    readonly description: string;
+    
+    @IsString()
+    @IsNotEmpty()
+    readonly eventPoster string;
+    
+    @IsString()
+    @MaxLength(10)
+    @IsNotEmpty()
+    readonly date: string;
+   
+    @IsString()
+    @MaxLength(10)
+    @IsNotEmpty()
+    readonly startTime: string;
+    
+    @IsString()
+    @MaxLength(10)
+    @IsNotEmpty()
+    readonly endTime: string;
+    
+    @IsNotEmpty()
+    readonly location: Record<number, unknown>;
+    
+    @IsString()
+    @MaxLength(30)
+    @IsNotEmpty()
+    readonly category: string; 
+
+    @IsString()
+    @MaxLength(30)
+    @IsNotEmpty()
+    readonly region: string;
+
+}
