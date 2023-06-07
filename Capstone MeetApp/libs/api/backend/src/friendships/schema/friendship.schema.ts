974723b70ffa9e7fdcee76cc25895cbf1f174f90
LesediMoko <lesedimokonyama@gmail.com>
2023-06-07 09:13:40 +0200

    friendship schema added

---
+++ Capstone MeetApp/libs/api/backend/src/friendships/schema/friendship.schema.ts
@@ -0,0 +1,27 @@
+/* eslint-disable prettier/prettier */
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+//import { Category } from 'src/utils/enums';
+//import { Organisation } from '../entities/organisation.entity';
+//import { ILocation } from 'src/utils/interfaces/location.interface';
+
+export type FriendshipDocument = HydratedDocument<Friendship>;
+
+@Schema({collection : "Friendships"})
+export class Friendship {
+
+  @Prop()
+  ID: mongoose.Schema.Types.ObjectId;
+
+  @Prop(/*{type: mongoose.Schema.Types.ObjectId, ref: 'Users'}*/)
+  requester: string;
+
+  @Prop(/*{type: mongoose.Schema.Types.ObjectId, ref: 'Users'}*/)
+  requestee: string;
+  @Prop()
+  status: string;
+
+ 
+}
+
+export const FriendshipSchema = SchemaFactory.createForClass(Friendship);
---
+++ Capstone MeetApp/libs/api/backend/src/friendships/schema/index.ts
@@ -0,0 +1,3 @@
+/* eslint-disable prettier/prettier */
+
+export * from './friendship.schema';
