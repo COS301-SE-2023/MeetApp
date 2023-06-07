aebd3a8f6df14427f7e18c6e5911bc5a9efe3f14
LesediMoko <lesedimokonyama@gmail.com>
2023-06-07 09:22:31 +0200

    attendances service added

---
+++ Capstone MeetApp/libs/api/backend/src/attendances/attendances.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreateAttendanceDto } from './dto/create-attendance.dto';
+import { UpdateAttendanceDto } from './dto/update-attendance.dto';
+
+@Injectable()
+export class AttendancesService {
+  create(createAttendanceDto: CreateAttendanceDto) {
+    return 'This action adds a new attendance';
+  }
+
+  findAll() {
+    return `This action returns all attendances`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} attendance`;
+  }
+
+  update(id: number, updateAttendanceDto: UpdateAttendanceDto) {
+    return `This action updates a #${id} attendance`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} attendance`;
+  }
+}
