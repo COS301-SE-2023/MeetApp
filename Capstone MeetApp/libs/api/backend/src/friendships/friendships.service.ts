4db58c1d29efb73ca5f129f657d6962a70750963
LesediMoko <lesedimokonyama@gmail.com>
2023-06-07 09:16:37 +0200

    friendship api service added

---
+++ Capstone MeetApp/libs/api/backend/src/friendships/friendships.service.ts
@@ -0,0 +1,51 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { CreateFriendshipDto } from './dto/create-friendship.dto';
+import { UpdateFriendshipDto } from './dto/update-friendship.dto';
+import { Friendship } from './schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { FilterQuery, Model } from 'mongoose';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class FriendshipsService {
+  constructor(@InjectModel(Friendship.name) private friendshipModel: Model<Friendship>, private userService :UsersService){
+    
+  }
+
+  create(createFriendshipDto: CreateFriendshipDto) {
+    return 'This action adds a new friendship';
+  }
+
+  async findAll() {
+    const friendDoc = await this.friendshipModel.find().exec();
+    const res = [];
+    const friendPromises = friendDoc.map(async (currentFriendship) => {
+      const currentFriendshipRequestee = (await this.userService.findOne(currentFriendship.requestee)).toObject()
+      //console.log(currentFriendship.requestee)
+      const currentFriendshipRequester = (await this.userService.findOne(currentFriendship.requester)).toObject()
+      const currentFriendshipStatus = currentFriendship.status
+      res.push({requester :currentFriendshipRequester, requestee : currentFriendshipRequestee, status : currentFriendshipStatus})
+  })
+  const friendshipRes = await Promise.all(friendPromises);
+  console.log(friendshipRes);
+
+  return res;
+  }
+
+  findByQuery(queryIN : FilterQuery<Event>) {
+    return this.friendshipModel.find(queryIN).exec();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} friendship`;
+  }
+
+  update(id: number, updateFriendshipDto: UpdateFriendshipDto) {
+    return `This action updates a #${id} friendship`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} friendship`;
+  }
+}
