c43ccc0a14687a70276e5efde79d7fcc1014efb8
AkaniHlungwani <u19240202@tuks.co.za>
2023-06-09 02:43:36 +0200

    Added create-organisation dto

--- Capstone MeetApp/libs/api/backend/src/organisations/dto/create-organisation.dto.ts
+++ Capstone MeetApp/libs/api/backend/src/organisations/dto/create-organisation.dto.ts
@@ -1,1 +1,35 @@
-export class CreateOrganisationDto {}
+import { IsNotEmpty, IsString, MaxLength ,IsArray} from "class-validator";
+
+export class CreateOrganisationDto {
+
+    @IsNotEmpty()
+    readonly ID: mongoose.Schema.Types.ObjectId;
+
+    @IsString()
+    @MaxLength(30)
+    @IsNotEmpty()
+    readonly username: string;
+
+    @IsString()
+    @MaxLength(30)
+    @IsNotEmpty()
+    readonly password: string;
+
+    @IsString()
+    @MaxLength(30)
+    @IsNotEmpty()
+    readonly name: string;
+
+    @IsString()
+    @MaxLength(30)
+    @IsNotEmpty()
+    readonly email: string;
+
+    @IsNotEmpty() 
+    readonly location: Record<number, unknown>;
+
+    @IsArray()
+    @IsNotEmpty() 
+    readonly events: string[];
+
+}
