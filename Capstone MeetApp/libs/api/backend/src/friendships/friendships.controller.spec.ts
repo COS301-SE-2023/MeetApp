696fc32711ce7e137e9b73151826054db291f665
LesediMoko <lesedimokonyama@gmail.com>
2023-06-10 02:53:33 +0200

    build fixes (missing providers in test files)

--- Capstone MeetApp/libs/api/backend/src/friendships/friendships.controller.spec.ts
+++ Capstone MeetApp/libs/api/backend/src/friendships/friendships.controller.spec.ts
@@ -1,6 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FriendshipsController } from './friendships.controller';
 import { FriendshipsService } from './friendships.service';
+import { UsersService } from '../users/users.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Friendship } from './schema';
+import { User } from '../users/schema';
 
 describe('FriendshipsController', () => {
   let controller: FriendshipsController;
@@ -8,7 +12,7 @@ describe('FriendshipsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FriendshipsController],
-      providers: [FriendshipsService],
+      providers: [FriendshipsService, UsersService, { provide: getModelToken(Friendship.name), useValue: jest.fn() }, { provide: getModelToken(User.name), useValue: jest.fn() }],
     }).compile();
 
     controller = module.get<FriendshipsController>(FriendshipsController);
--- Capstone MeetApp/libs/api/backend/src/friendships/friendships.service.spec.ts
+++ Capstone MeetApp/libs/api/backend/src/friendships/friendships.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FriendshipsService } from './friendships.service';
+import { UsersService } from '../users/users.service';
+import { User } from '../users/schema';
+import { Friendship } from './schema';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('FriendshipsService', () => {
   let service: FriendshipsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FriendshipsService],
+      providers: [FriendshipsService, UsersService, { provide: getModelToken(Friendship.name), useValue: jest.fn() }, { provide: getModelToken(User.name), useValue: jest.fn() }],
     }).compile();
 
     service = module.get<FriendshipsService>(FriendshipsService);
