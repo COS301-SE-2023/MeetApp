04be4e0055cc0ff9968e7470e6632f0cf2865fc8
KMAN-THECHEQUEBOOK <u19061430@tuks.co.za>
2023-06-22 18:56:09 +0200

    want to change branches
    want to change branches

--- Capstone MeetApp/libs/app/profile/feature/src/lib/profile/profile.component.ts
+++ Capstone MeetApp/libs/app/profile/feature/src/lib/profile/profile.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'capstone-meet-app-profile',
   standalone: true,
@@ -8,4 +8,11 @@ import { CommonModule } from '@angular/common';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+
+  constructor(private router: Router) {}
+
+  EditProfile(): void {
+    this.router.navigate(['/home']);
+  }
+}
