6e3dc7ec8a9e67435d396f2f7ce88ae35e981675
KMAN-THECHEQUEBOOK <u19061430@tuks.co.za>
2023-06-25 16:41:14 +0200

    90% complete create event page
    90% complete create event page

--- Capstone MeetApp/libs/app/organiser/feature/src/lib/organiser/organiser.component.ts
+++ Capstone MeetApp/libs/app/organiser/feature/src/lib/organiser/organiser.component.ts
@@ -6,6 +6,7 @@ import { CommonModule } from '@angular/common';
 import { FormControl } from '@angular/forms';
 import { AlertController } from '@ionic/angular';
 
+
 @Component({
   standalone: true,
   selector: 'capstone-meet-app-organiser',
@@ -16,16 +17,19 @@ import { AlertController } from '@ionic/angular';
     FormsModule,]
 })
 export class OrganiserComponent {
-  profilePictureUrl = 'https://images.unsplash.com/photo-1492684223066-81342ee5ff30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZXZlbnR8ZW58MHx8MHx8fDA%3D&w=1000&q=80';
+  profilePictureUrl: string | null = null
+  description: string | null = null
+  selectedRegion:string | null = null;
+  eventName :string | null = null;
+  
+ 
   selectedRange: { startDate: string; endDate: string } = {
     startDate: '',
     endDate: '',
   };
   startDateControl = new FormControl();
   endDateControl = new FormControl();
-  description = '';
-  selectedRegion= '';
-  eventName = '';
+  
   ngOnInit() {
     this.startDateControl.setValue(this.selectedRange.startDate);
     this.endDateControl.setValue(this.selectedRange.endDate);
@@ -41,14 +45,43 @@ export class OrganiserComponent {
       },
     ],
   };
-  constructor(private alertController: AlertController) {}
-
-  changeProfilePicture() {
-    // Logic to change the profile picture
-    // You can use a plugin or custom code to handle image selection and uploading
-    // Update the this.profilePictureUrl with the new image URL
+  constructor(private alertController: AlertController) {
+    this.profilePictureUrl = 'https://images.unsplash.com/photo-1492684223066-81342ee5ff30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZXZlbnR8ZW58MHx8MHx8fDA%3D&w=1000&q=80';
+    this.description='';
+    this.selectedRegion='';
+    this.eventName='';
   }
 
+  changeProfilePicture() {
+    const input = document.createElement('input');
+    input.type = 'file';
+  
+    input.click();
+  
+    input.addEventListener('change', (event) => {
+      const target = event.target as HTMLInputElement;
+      if (target.files && target.files.length > 0) {
+        const file = target.files[0];
+        const reader = new FileReader();
+  
+        reader.readAsDataURL(file);
+  
+        reader.addEventListener('load', (event) => {
+          const profilePictureUrl = event.target?.result as string;
+          if (profilePictureUrl) {
+            this.saveProfilePicture(profilePictureUrl);
+          }
+        });
+      }
+    });
+  }
+  
+  
+    saveProfilePicture(profilePictureUrl: string) {
+      this.profilePictureUrl=profilePictureUrl;
+      console.log('Profile picture URL:', profilePictureUrl);
+    }
+  
   async saveProfile() {
     if (
       this.selectedRange.startDate &&
@@ -63,11 +96,7 @@ export class OrganiserComponent {
       console.log('Selected Region:', this.selectedRegion);
       console.log('Selected Region:', this.eventName);
   
-      // Perform further actions with the selected data
-  
-      // Logic to change the profile picture
-      // You can use a plugin or custom code to handle image selection and uploading
-      // Update the this.profilePictureUrl with the new image URL
+      
     } else {
       const alert = await this.alertController.create({
         header: 'Incomplete Fields',
