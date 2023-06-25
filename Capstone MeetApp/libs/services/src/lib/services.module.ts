910be3ac4dc2c819665e31fba05a5b736966f156
Jacob-Mahloko <114677637+Jacob-Mahloko@users.noreply.github.com>
2023-06-25 20:58:08 +0200

    🐛 Bug fix

--- Capstone MeetApp/libs/services/src/lib/services.module.ts
+++ Capstone MeetApp/libs/services/src/lib/services.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {service} from 'libs/services/src/lib/servises.service';
+import { service } from './servises.service';
 
 @NgModule({
   imports: [CommonModule,],
