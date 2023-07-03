7f0885c3fc143a4cc8a3702da12e36d60108337c
Jacob-Mahloko <114677637+Jacob-Mahloko@users.noreply.github.com>
2023-07-03 18:10:53 +0200

    💄 Settings UI

--- Capstone MeetApp/libs/app/settings/feature/src/lib/settings/settings.component.ts
+++ Capstone MeetApp/libs/app/settings/feature/src/lib/settings/settings.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular'; 
+import { IonicModule } from '@ionic/angular';
+import { service } from '@capstone-meet-app/app/services'; 
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 @Component({
   standalone:true,
   selector: 'capstone-meet-app-settings',
-  imports: [ IonicModule],
+  imports: [ IonicModule,CommonModule],
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css'],
+  providers:[service,Router,],
 })
-export class SettingsComponent {}
+export class SettingsComponent {
+  constructor(private service:service,private router:Router){
+
+  }
+
+  navigateToProfile(){
+    this.router.navigate(['/profile']);
+  }
+
+  nagivateToHome(): void {
+    this.router.navigate(['/home']);
+  }
+}
