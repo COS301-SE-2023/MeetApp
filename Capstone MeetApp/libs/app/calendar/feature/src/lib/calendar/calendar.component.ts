84d7c00123361e214eda5f03d01cb5ee122c4321
Jacob-Mahloko <114677637+Jacob-Mahloko@users.noreply.github.com>
2023-07-04 19:26:45 +0200

    ⚡️ added services to the calendar

--- Capstone MeetApp/libs/app/calendar/feature/src/lib/calendar/calendar.component.ts
+++ Capstone MeetApp/libs/app/calendar/feature/src/lib/calendar/calendar.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {IonicModule } from '@ionic/angular';
-import {service} from '@capstone-meet-app/app/services'
+import {service,events} from '@capstone-meet-app/app/services'
 @Component({
   selector: 'capstone-meet-app-calendar',
   standalone: true,
@@ -14,4 +14,34 @@ export class CalendarComponent {
   constructor(private service:service){
 
   }
+
+  data= [{
+    name:'',
+    organisation: '',
+    date: '',
+    startTime: '',
+    endTime: '',
+    eventDate: '',
+    lng: 0,
+    lat: 0,
+    location: {latitude:'' , longitude:''},
+    category:'',
+    region:'',
+    description:''
+}
+  
+];
+  async ngOnInit() {
+    this.service.getAllEvents().subscribe((response: any) => { 
+      this.data = response;
+      for (let i = 0; i < this.data.length; i++) {
+        const event: events = this.data[i];
+        const region = event.region;
+        const date=event.date;
+        console.log(date);
+       
+      }
+      this.data
+    });
+}
 }
