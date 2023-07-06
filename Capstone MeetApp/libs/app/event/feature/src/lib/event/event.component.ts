3b67b9df06e0d7a32e363908a1146c3de1a5abdf
Jacob-Mahloko <114677637+Jacob-Mahloko@users.noreply.github.com>
2023-07-06 05:34:00 +0200

    💄 added proper css

--- Capstone MeetApp/libs/app/event/feature/src/lib/event/event.component.ts
+++ Capstone MeetApp/libs/app/event/feature/src/lib/event/event.component.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { IonicModule } from '@ionic/angular'; 
+import {service,events} from '@capstone-meet-app/app/services';
+import { Router } from '@angular/router';
 @Component({
   selector: 'capstone-meet-app-event',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,IonicModule],
   templateUrl: './event.component.html',
   styleUrls: ['./event.component.css'],
+  providers:[service,Router]
 })
-export class EventComponent {}
+export class EventComponent {
+  constructor(private service:service,private router:Router){
+
+  }
+  data= [{
+    name:'',
+    organisation: '',
+    date: '',
+    startTime: '',
+    endTime: '',
+    eventDate: '',
+    lng: 0,
+    lat: 0,
+    location: {latitude:'' , longitude:''},
+    category:'',
+    region:'',
+    description:''
+}
+  
+];
+  async ngOnInit() {
+    this.service.getAllEvents().subscribe((response: any) => { 
+      this.data = response;
+      for (let i = 0; i < this.data.length; i++) {
+        const event: events = this.data[i];
+        const region = event.region;
+        const date=event.date;
+        console.log(date);
+       
+      }
+      this.data
+    });
+
+  }
+}
