c6e8cf1c13647d0b224bb4284f4fbd1a4dff1204
LesediMoko <lesedimokonyama@gmail.com>
2023-07-10 05:43:23 +0200

    users gaurd import in controller

--- Capstone MeetApp/libs/api/backend/src/users/users.controller.ts
+++ Capstone MeetApp/libs/api/backend/src/users/users.controller.ts
@@ -4,6 +4,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Request } from 'express';
 import { JwtModule } from '@nestjs/jwt';
+import { AuthGuard } from './users.guard';
 
 @Controller('users')
 export class UsersController {
