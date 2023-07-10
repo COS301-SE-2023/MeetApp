4960aca0e54d6f41a377c0e29d72fcf20c07e336
LesediMoko <lesedimokonyama@gmail.com>
2023-07-10 05:36:58 +0200

    jwt payload

--- Capstone MeetApp/libs/api/backend/src/users/users.controller.ts
+++ Capstone MeetApp/libs/api/backend/src/users/users.controller.ts
@@ -3,6 +3,7 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Request } from 'express';
+import { JwtModule } from '@nestjs/jwt';
 
 @Controller('users')
 export class UsersController {
