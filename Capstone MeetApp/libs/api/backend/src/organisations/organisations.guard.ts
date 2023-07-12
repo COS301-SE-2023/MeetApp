ae90b00e4838f0a95deffba970d13810347049dc
LesediMoko <lesedimokonyama@gmail.com>
2023-07-12 19:44:50 +0200

    org guard comment removal

--- Capstone MeetApp/libs/api/backend/src/organisations/organisations.guard.ts
+++ Capstone MeetApp/libs/api/backend/src/organisations/organisations.guard.ts
@@ -25,8 +25,6 @@ import {
             secret: jwtConstants.secret
           }
         );
-        // 💡 We're assigning the payload to the request object here
-        // so that we can access it in our route handlers
         request['user'] = payload;
       } catch {
         throw new UnauthorizedException();
