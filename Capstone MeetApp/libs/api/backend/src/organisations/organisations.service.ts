a88efd8c0c5600861b4366ca2462d8a9fb06eb7a
LesediMoko <lesedimokonyama@gmail.com>
2023-07-12 19:51:22 +0200

    org.service for JWT login done

--- Capstone MeetApp/libs/api/backend/src/organisations/organisations.service.ts
+++ Capstone MeetApp/libs/api/backend/src/organisations/organisations.service.ts
@@ -5,10 +5,11 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Organisation } from './schema';
 import { Model } from 'mongoose';
 import { EventsService } from '../events/events.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class OrganisationsService {
-  constructor(@InjectModel(Organisation.name) private organisationModel: Model<Organisation>, private eventService :EventsService){
+  constructor(@InjectModel(Organisation.name) private organisationModel: Model<Organisation>, private eventService :EventsService, private jwtService: JwtService){
     
   }
   async create(createOrgDto: CreateOrganisationDto) {
@@ -22,7 +23,8 @@ export class OrganisationsService {
     }
     else {
       if (orgToLoginInto[0].password == password){
-        return {organisation: orgToLoginInto[0], message : 'Login successful'}
+        const payload = {id : orgToLoginInto[0].id, username : orgToLoginInto[0].username, password: orgToLoginInto[0].password}
+        return {access_token: await this.jwtService.signAsync(payload),message : 'Login successful'}
       }
       else{
         return {organisation: username, message : 'Incorrect password'}
