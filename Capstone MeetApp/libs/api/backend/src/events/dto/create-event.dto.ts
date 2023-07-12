b1a4fab063e155552a8337d60f9e34c00a83f024
Akani Hlungwani <116602464+AkaniHlungwani@users.noreply.github.com>
2023-07-12 20:42:21 +0200

    reverted dto and schema changes

--- Capstone MeetApp/libs/api/backend/src/events/dto/create-event.dto.ts
+++ Capstone MeetApp/libs/api/backend/src/events/dto/create-event.dto.ts
@@ -1,14 +1,21 @@
 export class CreateEventDto {
    
     readonly name!: string;
+    
     readonly organisation!: string;
+    
     readonly description!: string;
+
     readonly date!: string;
+   
     readonly startTime!: string;
+ 
     readonly endTime!: string;
-    readonly location!: {latitude:string , longitude:string};
+    
+    readonly location!: {latitude:number , longitude:number};
+
     readonly category!: string; 
+
     readonly region!: string;
-    readonly eventPoster!:string;
 
 }
--- Capstone MeetApp/libs/api/backend/src/organisations/dto/create-organisation.dto.ts
+++ Capstone MeetApp/libs/api/backend/src/organisations/dto/create-organisation.dto.ts
@@ -1,12 +1,10 @@
 export class CreateOrganisationDto {
     
-    readonly name!:string;
-    readonly surname!:string;
     readonly username!: string;
-    readonly email!:string;
+
     readonly password!: string;
-    readonly phoneNumber!:string;
-    readonly orgDescription!:string;
+
+    readonly name!: string;
+
     readonly events!: string[];
-    
 }
