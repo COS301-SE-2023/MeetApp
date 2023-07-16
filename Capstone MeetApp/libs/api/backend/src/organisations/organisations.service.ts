c03501d81332575cf5358b72b24ae94e12835a5a
LesediMoko <lesedimokonyama@gmail.com>
2023-07-16 23:35:02 +0200

    code cleanup (removal of logs and comments)

--- Capstone MeetApp/libs/api/backend/src/organisations/organisations.service.ts
+++ Capstone MeetApp/libs/api/backend/src/organisations/organisations.service.ts
@@ -55,15 +55,12 @@ export class OrganisationsService {
   }
 
   async getTop3AttendedEvents(organizationId: string) {
-    // Find events for the specified organization
-    //const org = await this.findOne(organizationId)
     const events = await this.findEvents(organizationId);
 
     if (!events) {
       throw new NotFoundException('Organization not found.');
     }
 
-    // Sort events by attendance count in descending order
     const sortedEvents = await Promise.all(
       events.map(async (event) => {
         const attendanceCount = await this.attendanceModel.countDocuments({ eventID: event?.ID }).exec();
@@ -72,22 +69,18 @@ export class OrganisationsService {
     );
     sortedEvents.sort((a, b) => b.attendanceCount - a.attendanceCount);
 
-    // Return the top 3 most attended events
     const topAttendedEvents = sortedEvents.slice(0, 3).map((item) => item.event);
 
     return topAttendedEvents;
   }
 
   async getTopAttendedEvent(organizationId: string) {
-    // Find events for the specified organization
-    //const org = await this.findOne(organizationId)
     const events = await this.findEvents(organizationId);
 
     if (!events) {
       throw new NotFoundException('Organization not found.');
     }
 
-    // Sort events by attendance count in descending order
     const sortedEvents = await Promise.all(
       events.map(async (event) => {
         const attendanceCount = await this.attendanceModel.countDocuments({ eventID: event?.ID }).exec();
@@ -96,27 +89,23 @@ export class OrganisationsService {
     );
     sortedEvents.sort((a, b) => b.attendanceCount - a.attendanceCount);
 
-    // Return the top 3 most attended events
     const topAttendedEvents = sortedEvents.slice(0, 3).map((item) => item.event);
 
     return topAttendedEvents[0];
   }
 
   async getTop3EventCategories(organizationId: string): Promise<string[]> {
-    // Find events for the specified organization
     const events = await this.findEvents(organizationId );
 
     if (!events) {
       throw new NotFoundException('Organization not found.');
     }
-    // Calculate the count of events for each category
     const categoryCounts: { [key: string]: number } = {};
     events.forEach((event) => {
       if (event != null){
       const category = event.category;
       categoryCounts[category] = (categoryCounts[category] || 0) + 1;}
     });
-    console.log(categoryCounts);
 
     const sortedCategories = Object.keys(categoryCounts).sort(
       (a, b) => categoryCounts[b] - categoryCounts[a]
@@ -128,20 +117,17 @@ export class OrganisationsService {
   }
 
   async getTopEventCategory(organizationId: string): Promise<string> {
-    // Find events for the specified organization
     const events = await this.findEvents(organizationId );
 
     if (!events) {
       throw new NotFoundException('Organization not found.');
     }
-    // Calculate the count of events for each category
     const categoryCounts: { [key: string]: number } = {};
     events.forEach((event) => {
       if (event != null){
       const category = event.category;
       categoryCounts[category] = (categoryCounts[category] || 0) + 1;}
     });
-    console.log(categoryCounts);
 
     const sortedCategories = Object.keys(categoryCounts).sort(
       (a, b) => categoryCounts[b] - categoryCounts[a]
@@ -153,20 +139,17 @@ export class OrganisationsService {
   }
 
   async getTopEventRegion(organizationId: string): Promise<string> {
-    // Find events for the specified organization
     const events = await this.findEvents(organizationId );
 
     if (!events) {
       throw new NotFoundException('Organization not found.');
     }
-    // Calculate the count of events for each category
     const regionCounts: { [key: string]: number } = {};
     events.forEach((event) => {
       if (event != null){
       const region = event.region;
       regionCounts[region] = (regionCounts[region] || 0) + 1;}
     });
-    console.log(regionCounts);
 
     const sortedRegions = Object.keys(regionCounts).sort(
       (a, b) => regionCounts[b] - regionCounts[a]
@@ -178,20 +161,17 @@ export class OrganisationsService {
   }
 
   async getTop3EventRegion(organizationId: string): Promise<string[]> {
-    // Find events for the specified organization
     const events = await this.findEvents(organizationId );
 
     if (!events) {
       throw new NotFoundException('Organization not found.');
     }
-    // Calculate the count of events for each category
     const regionCounts: { [key: string]: number } = {};
     events.forEach((event) => {
       if (event != null){
       const region = event.region;
       regionCounts[region] = (regionCounts[region] || 0) + 1;}
     });
-    console.log(regionCounts);
 
     const sortedRegions = Object.keys(regionCounts).sort(
       (a, b) => regionCounts[b] - regionCounts[a]
@@ -203,7 +183,6 @@ export class OrganisationsService {
   }
 
   async getTop3Supporters(organizationId: string): Promise<{ username: string, region: string }[]> {
-    // Find attendees for events hosted by the organization
     const attendees = await this.attendanceModel
       .find({ organisationID: organizationId })
       .select('userID')
@@ -219,12 +198,10 @@ export class OrganisationsService {
       userCounts[userId] = (userCounts[userId] || 0) + 1;
     });
 
-    // Sort users by attendance count in descending order
     const sortedUsers = Object.keys(userCounts).sort(
       (a, b) => userCounts[b] - userCounts[a]
     );
 
-    // Retrieve user details for the top 3 supporters
     const topSupporterIds = sortedUsers.slice(0, 3);
     const topSupporters = await this.userModel.find({ _id: { $in: topSupporterIds } }).select('username region -_id').exec();
 
@@ -232,7 +209,6 @@ export class OrganisationsService {
   }
 
   async getTopSupporter(organizationId: string): Promise<{ username: string, region: string }> {
-    // Find attendees for events hosted by the organization
     const attendees = await this.attendanceModel
       .find({ organisationID: organizationId })
       .select('userID')
@@ -248,12 +224,10 @@ export class OrganisationsService {
       userCounts[userId] = (userCounts[userId] || 0) + 1;
     });
 
-    // Sort users by attendance count in descending order
     const sortedUsers = Object.keys(userCounts).sort(
       (a, b) => userCounts[b] - userCounts[a]
     );
 
-    // Retrieve user details for the top 3 supporters
     const topSupporterIds = sortedUsers.slice(0, 3);
     const topSupporters = await this.userModel.find({ _id: { $in: topSupporterIds } }).select('username region -_id').exec();
 
