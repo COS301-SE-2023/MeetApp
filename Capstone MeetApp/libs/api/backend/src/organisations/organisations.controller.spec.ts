678396808ecceba0350ce3990d11cb820f9e9730
LesediMoko <lesedimokonyama@gmail.com>
2023-07-20 22:56:14 +0200

    org controller spec file update

--- Capstone MeetApp/libs/api/backend/src/organisations/organisations.controller.spec.ts
+++ Capstone MeetApp/libs/api/backend/src/organisations/organisations.controller.spec.ts
@@ -6,6 +6,7 @@ import { getModelToken } from '@nestjs/mongoose';
 import { EventsService } from '../events/events.service';
 import { Event } from '../events/schema';
 import { Attendance } from '../attendances/schema';
+import { User } from '../users/schema';
 
 describe('OrganisationsController', () => {
   let controller: OrganisationsController;
@@ -13,7 +14,7 @@ describe('OrganisationsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrganisationsController],
-      providers: [OrganisationsService, EventsService, { provide: getModelToken(Organisation.name), useValue: jest.fn() }, { provide: getModelToken(Event.name), useValue: jest.fn() }, { provide: getModelToken(Attendance.name), useValue: jest.fn() }],
+      providers: [OrganisationsService, EventsService, { provide: getModelToken(Organisation.name), useValue: jest.fn() }, { provide: getModelToken(Event.name), useValue: jest.fn() }, { provide: getModelToken(Attendance.name), useValue: jest.fn() }, { provide: getModelToken(User.name), useValue: jest.fn() }],
     }).compile();
 
     controller = module.get<OrganisationsController>(OrganisationsController);
