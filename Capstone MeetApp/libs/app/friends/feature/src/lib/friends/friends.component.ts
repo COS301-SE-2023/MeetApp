4af9b6a52db825635041ca0a72bdf798b35b13e0
KMAN-THECHEQUEBOOK <u19061430@tuks.co.za>
2023-07-28 01:31:41 +0200

    solving conflicts
    solving conflicts

--- Capstone MeetApp/libs/app/friends/feature/src/lib/friends/friends.component.ts
+++ Capstone MeetApp/libs/app/friends/feature/src/lib/friends/friends.component.ts
@@ -1,13 +1,64 @@
 import { Component } from '@angular/core';
+
+import { CommonModule,Location } from '@angular/common';
+import {IonicModule } from '@ionic/angular';
+//import {service,events} from '@capstone-meet-app/app/services'
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+
 import { events,service,ServicesModule} from '@capstone-meet-app/services';
+
 @Component({
   standalone:true,
   selector: 'capstone-meet-app-friends',
   templateUrl: './friends.component.html',
+  imports: [CommonModule, FormsModule,IonicModule],
   styleUrls: ['./friends.component.css'],
   providers:[service],
 })
 export class FriendsComponent {
+
+  
+  followers= [{
+    name:'shiluvelo',
+    profilepicture:'assets/profile.png'
+  },
+  {
+    name:'shiluvelo',
+    profilepicture:'assets/profile.png'
+  },
+  {
+    name:'shiluvelo',
+    profilepicture:'assets/profile.png'
+  },
+  {
+    name:'shiluvelo',
+    profilepicture:'assets/profile.png'
+  },
+  {
+    name:'shiluvelo',
+    profilepicture:'assets/profile.png'
+  }
+];
+
+
+//goBack() {
+ // this.location.back();
+//}
+filteredData: any[] = [];
+    searchQuery = '';
+search(): void {
+  if (this.searchQuery.trim() === '') {
+    this.filteredData = this.followers;
+  } else {
+    this.filteredData = this.followers.filter((item) =>
+      item.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+    );
+  }
+ }
+
+
   status: string|undefined
 constructor(private servicesService: service) {}
 
@@ -54,5 +105,3 @@ async deletefriend() {
 
 }
 }
-
-
