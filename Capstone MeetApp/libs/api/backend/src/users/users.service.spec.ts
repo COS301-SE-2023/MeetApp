7ea05374acf4ea58c57f9f123edcfa12c691701c
LesediMoko <lesedimokonyama@gmail.com>
2023-07-29 23:22:58 +0200

    user service spec file fixed

--- Capstone MeetApp/libs/api/backend/src/users/users.service.spec.ts
+++ Capstone MeetApp/libs/api/backend/src/users/users.service.spec.ts
@@ -7,13 +7,14 @@ import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constant';
 import { Organisation } from '../organisations/schema';
 import { Event } from '../events/schema';
+import { Friendship } from '../friendships/schema';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService, { provide: getModelToken(User.name), useValue: jest.fn() }, { provide: getModelToken(Attendance.name), useValue: jest.fn() }, { provide: getModelToken(Event.name), useValue: jest.fn() }, { provide: getModelToken(Organisation.name), useValue: jest.fn() }],
+      providers: [UsersService, { provide: getModelToken(User.name), useValue: jest.fn() }, { provide: getModelToken(Attendance.name), useValue: jest.fn() }, { provide: getModelToken(Event.name), useValue: jest.fn() }, { provide: getModelToken(Organisation.name), useValue: jest.fn() }, { provide: getModelToken(Friendship.name), useValue: jest.fn() }],
       imports: [
         JwtModule.register({
           global: true,
