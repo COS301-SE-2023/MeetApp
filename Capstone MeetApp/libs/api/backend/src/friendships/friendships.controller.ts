348d89db7978bb923f28f9ad246eb3eeaff0820f
LesediMoko <lesedimokonyama@gmail.com>
2023-08-06 01:23:11 +0200

    code cleanup

--- Capstone MeetApp/libs/api/backend/src/friendships/friendships.controller.ts
+++ Capstone MeetApp/libs/api/backend/src/friendships/friendships.controller.ts
@@ -1,7 +1,5 @@
 import { Controller, Get, Param, Req } from '@nestjs/common';
 import { FriendshipsService } from './friendships.service';
-// import { CreateFriendshipDto } from './dto/create-friendship.dto';
-// import { UpdateFriendshipDto } from './dto/update-friendship.dto';
 import { Request } from 'express';
 import { ApiOperation, ApiResponse, ApiParam, ApiTags } from '@nestjs/swagger';
 
@@ -9,11 +7,6 @@ import { ApiOperation, ApiResponse, ApiParam, ApiTags } from '@nestjs/swagger';
 @ApiTags('Friendships')
 export class FriendshipsController {
   constructor(private readonly friendshipsService: FriendshipsService) {}
-
-  // @Post()
-  // create(@Body() createFriendshipDto: CreateFriendshipDto) {
-  //   return this.friendshipsService.create(createFriendshipDto);
-  // }
 
   @Get()
   @ApiOperation({ summary: 'Get all friendships (filtered by query)' }) 
@@ -30,13 +23,4 @@ export class FriendshipsController {
     return this.friendshipsService.findOne(+id);
   }
 
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateFriendshipDto: UpdateFriendshipDto) {
-  //   return this.friendshipsService.update(+id, updateFriendshipDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.friendshipsService.remove(+id);
-  // }
 }
