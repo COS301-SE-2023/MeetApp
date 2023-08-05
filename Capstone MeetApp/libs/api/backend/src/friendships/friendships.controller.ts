0151c691ac70517783dba5fd40b5120483213d89
LesediMoko <lesedimokonyama@gmail.com>
2023-08-06 01:22:26 +0200

    friendship endpoints decorated with swagger

--- Capstone MeetApp/libs/api/backend/src/friendships/friendships.controller.ts
+++ Capstone MeetApp/libs/api/backend/src/friendships/friendships.controller.ts
@@ -3,7 +3,7 @@ import { FriendshipsService } from './friendships.service';
 // import { CreateFriendshipDto } from './dto/create-friendship.dto';
 // import { UpdateFriendshipDto } from './dto/update-friendship.dto';
 import { Request } from 'express';
-import { ApiOperation, ApiResponse, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiParam, ApiTags } from '@nestjs/swagger';
 
 @Controller('friendships')
 @ApiTags('Friendships')
@@ -16,11 +16,16 @@ export class FriendshipsController {
   // }
 
   @Get()
+  @ApiOperation({ summary: 'Get all friendships (filtered by query)' }) 
+  @ApiResponse({ status: 201, description: 'List of friendships filtered by query (if specified)' })
   findAll(@Req() request: Request) {
       return this.friendshipsService.findByQuery(request.query)
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a specfic friendship' }) 
+  @ApiResponse({ status: 201, description: 'Specified Friendship (requesterID, requesteeID)' })
+  @ApiParam({name: 'id', description: 'the id of the friendship'})
   findOne(@Param('id') id: string) {
     return this.friendshipsService.findOne(+id);
   }
