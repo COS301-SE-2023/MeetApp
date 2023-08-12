7cdfb1cb8278a623c747f49fd3629b9da767e888
shiluvelomakhubele <117033636+ShiluveloMakhubel@users.noreply.github.com>
2023-08-12 12:27:57 +0200

    attendees ui

---
+++ Capstone MeetApp/libs/app/attendees/src/lib/app-attendees/app-attendees.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'capstone-meet-app-app-attendees',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app-attendees.component.html',
+  styleUrls: ['./app-attendees.component.css'],
+})
+export class AppAttendeesComponent {}
