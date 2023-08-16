35b38888049cde711689f6a1afc2f59f3b93d099
LesediMoko <lesedimokonyama@gmail.com>
2023-08-17 01:26:23 +0200

    interface change to class. userloginrequest done

--- Capstone MeetApp/libs/api/backend/src/interfaces.ts
+++ Capstone MeetApp/libs/api/backend/src/interfaces.ts
@@ -1,3 +1,14 @@
-export interface AuthenticatedRequest extends Request {
-    user: {id : string, username : string, password: string};
+import { ApiProperty } from "@nestjs/swagger";
+
+export class AuthenticatedRequest extends Request {
+    readonly user!: {id : string, username : string, password: string};
+  }
+
+export class UserLoginRequest {
+
+    @ApiProperty({description : 'An existing username.', example: 'john_doe'})
+     readonly username! : string;
+
+    @ApiProperty({description : 'The password associated with username.', example: 'pass123'})
+    readonly password! : string;
   }
