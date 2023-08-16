f89e32ebc6c3748e6e1aaff952981391612e1a78
LesediMoko <lesedimokonyama@gmail.com>
2023-08-17 01:21:52 +0200

    creatuser dto decorated

--- Capstone MeetApp/libs/api/backend/src/users/dto/create-user.dto.ts
+++ Capstone MeetApp/libs/api/backend/src/users/dto/create-user.dto.ts
@@ -1,7 +1,13 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateUserDto {
+  @ApiProperty({example: "user_man23", description : "The user's desired username."})
   readonly username!: string;
+  @ApiProperty({example: "password101", description : "The user's chosen password."})
   readonly password!: string;
+  @ApiProperty({example: "em=e?vftb+asi...", description : "Base64/BSON representation of profile picture."})
   readonly profilePicture!: string;
+  @ApiProperty({example: "Joburg", description : "The user's current region."})
   readonly region!: string;
   
 }
