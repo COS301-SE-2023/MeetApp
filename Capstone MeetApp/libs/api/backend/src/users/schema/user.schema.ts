975da4a3c4c3f4499103c958147b52162e0947e8
LesediMoko <lesedimokonyama@gmail.com>
2023-08-17 02:06:10 +0200

    users schema decorated

--- Capstone MeetApp/libs/api/backend/src/users/schema/user.schema.ts
+++ Capstone MeetApp/libs/api/backend/src/users/schema/user.schema.ts
@@ -1,4 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 import mongoose, { HydratedDocument } from 'mongoose';
 //import { Category } from 'src/utils/enums';
 //import { Organisation } from '../entities/organisation.entity';
@@ -9,18 +10,23 @@ export type UserDocument = HydratedDocument<User>;
 @Schema({collection : "Users"})
 export class User {
 
+  @ApiProperty({example: "747223dedd65fc64879e13dc", description : "The user's desired username."})
   @Prop()
   ID!: mongoose.Schema.Types.ObjectId;
 
+  @ApiProperty({example: "user_man23", description : "The user's desired username."})
   @Prop()
   username!: string;
 
+  @ApiProperty({example: "password101", description : "The user's chosen password."})
   @Prop()
   password!: string;
 
+  @ApiProperty({example: "em=e?vftb+asi...", description : "Base64/BSON representation of profile picture."})
   @Prop()
   profilePicture!: string;
 
+  @ApiProperty({example: "Joburg", description : "The user's current region."})
   @Prop()
   region!: string;
   
