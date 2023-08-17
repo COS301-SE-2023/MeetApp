7c0ee9019d44221316d24585f87aa2b6c525ef95
LesediMoko <lesedimokonyama@gmail.com>
2023-08-17 02:05:38 +0200

    auth request interface and class seperated

--- Capstone MeetApp/libs/api/backend/src/interfaces.ts
+++ Capstone MeetApp/libs/api/backend/src/interfaces.ts
@@ -1,8 +1,15 @@
+//import { Request } from "@nestjs/common";
 import { ApiProperty } from "@nestjs/swagger";
+import { Request } from '@nestjs/common';
 
-export class AuthenticatedRequest extends Request {
+export interface AuthenticatedRequest extends Request {
+    //@ApiProperty({description: "The user's credentials", example: {id: "747223dedd65fc64879e13dc", username: "TimothyJones24", password: "pass435"}})
+     user: {id : string, username : string, password: string};
+  }
+
+export class AuthenticatedRequestClass {
     @ApiProperty({description: "The user's credentials", example: {id: "747223dedd65fc64879e13dc", username: "TimothyJones24", password: "pass435"}})
-    readonly user!: {id : string, username : string, password: string};
+     readonly user!: {id : string, username : string, password: string};
   }
 
 export class UserLoginRequest {
