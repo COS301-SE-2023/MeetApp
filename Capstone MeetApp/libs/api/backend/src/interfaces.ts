e53831f44949fdabeea3de6b3162709637cd3870
LesediMoko <lesedimokonyama@gmail.com>
2023-08-17 02:13:13 +0200

    userfriends done and decorated

--- Capstone MeetApp/libs/api/backend/src/interfaces.ts
+++ Capstone MeetApp/libs/api/backend/src/interfaces.ts
@@ -1,6 +1,7 @@
 //import { Request } from "@nestjs/common";
 import { ApiProperty } from "@nestjs/swagger";
 import { Request } from '@nestjs/common';
+import mongoose from "mongoose";
 
 export interface AuthenticatedRequest extends Request {
     //@ApiProperty({description: "The user's credentials", example: {id: "747223dedd65fc64879e13dc", username: "TimothyJones24", password: "pass435"}})
@@ -19,4 +20,16 @@ export class UserLoginRequest {
 
     @ApiProperty({description : 'The password associated with username.', example: 'pass123'})
     readonly password! : string;
+  }
+
+  export class UserFriends {
+
+    @ApiProperty({example: "747223dedd65fc64879e13dc", description : "The user's desired username."})
+    readonly ID!: mongoose.Schema.Types.ObjectId;
+
+    @ApiProperty({description : 'An existing username.', example: 'john_doe'})
+    readonly username! : string;
+
+    @ApiProperty({example: "em=e?vftb+asi...", description : "Base64/BSON representation of profile picture."})
+    readonly profilePicture!: string;
   }
