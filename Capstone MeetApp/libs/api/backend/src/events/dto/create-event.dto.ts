2c1ec6b706540b7e4b74319d518e33ad9908f310
LesediMoko <lesedimokonyama@gmail.com>
2023-08-19 16:25:34 +0200

    create-event dto swaggerui updated

--- Capstone MeetApp/libs/api/backend/src/events/dto/create-event.dto.ts
+++ Capstone MeetApp/libs/api/backend/src/events/dto/create-event.dto.ts
@@ -2,34 +2,34 @@ import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateEventDto {
    
-    @ApiProperty({example: 'CyberSummit', description: 'The name of the event.'})
+    @ApiProperty({example: 'CyberSummit', description: 'The name of the event.', required: true})
     readonly name!: string;
     
-    @ApiProperty({example: 'TechSummit Corporation', description: 'The name of the organisation (existing) hosting the event.'})
+    @ApiProperty({example: 'TechSummit Corporation', description: 'The name of the organisation (existing) hosting the event.', required: true})
     readonly organisation!: string;
     
-    @ApiProperty({example: 'Join us as we embark on a journey through the world of networks and their number one enemy: Hackers!', description: 'A short description of the event.'})
+    @ApiProperty({example: 'Join us as we embark on a journey through the world of networks and their number one enemy: Hackers!', description: 'A short description of the event.', required: true})
     readonly description!: string;
 
-    @ApiProperty({example: 'as9frfr=cwc?Evt...', description: 'A Base64/BSON representation of the event poster image'})
+    @ApiProperty({example: 'as9frfr=cwc?Evt...', description: 'A Base64/BSON representation of the event poster image', required: true})
     readonly eventPoster!: string;
 
-    @ApiProperty({example: '2023-10-11', description: 'The date of the event in yyyy-mm-dd format.'})
+    @ApiProperty({example: '2023-10-11', description: 'The date of the event in yyyy-mm-dd format.', required: true})
     readonly date!: string;
    
-    @ApiProperty({example: '17:20', description: 'The time at which the event begins in hh:mm format (24h).'})
+    @ApiProperty({example: '17:20', description: 'The time at which the event begins in hh:mm format (24h).', required: true})
     readonly startTime!: string;
  
-    @ApiProperty({example: '20:00', description: 'The time at which the event ends in hh:mm format (24h).'})
+    @ApiProperty({example: '20:00', description: 'The time at which the event ends in hh:mm format (24h).', required: true})
     readonly endTime!: string;
     
-    @ApiProperty({example: {latitude : 51.5074, longitude : -0.1278}, description: 'The location of the event in longitude and latitude.', type : 'OrderedMap'})
+    @ApiProperty({example: {latitude : 51.5074, longitude : -0.1278}, description: 'The location of the event in longitude and latitude.', type : 'OrderedMap', required: true})
     readonly location!: {latitude:number , longitude:number};
 
-    @ApiProperty({example: 'Technology', description: 'The category of the event.'})
+    @ApiProperty({example: 'Technology', description: 'The category of the event.', required: true})
     readonly category!: string; 
 
-    @ApiProperty({example: 'Pretoria', description: 'Where the event will be hosted.'})
+    @ApiProperty({example: 'Pretoria', description: 'Where the event will be hosted.', required: true})
     readonly region!: string;
 
 }
