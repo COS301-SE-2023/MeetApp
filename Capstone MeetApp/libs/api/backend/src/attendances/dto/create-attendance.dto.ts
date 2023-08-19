61b15c3bbae9f00bb99a4855e1d2bdd564d08887
LesediMoko <lesedimokonyama@gmail.com>
2023-08-19 16:17:57 +0200

    create-attend dto updated

--- Capstone MeetApp/libs/api/backend/src/attendances/dto/create-attendance.dto.ts
+++ Capstone MeetApp/libs/api/backend/src/attendances/dto/create-attendance.dto.ts
@@ -1,9 +1,14 @@
+import { ApiProperty } from "@nestjs/swagger";
 import mongoose from "mongoose";
 
 export class CreateAttendanceDto {
 
-  readonly ID!: mongoose.Schema.Types.ObjectId;
+  @ApiProperty({description: "", example: "", required: true})
   readonly organisationID!: mongoose.Schema.Types.ObjectId;
+
+  @ApiProperty({description: "", example: "", required: true})
   readonly eventID!: mongoose.Schema.Types.ObjectId;
+
+  @ApiProperty({description: "", example: "", required: true})
   readonly userID!: mongoose.Schema.Types.ObjectId;
 }
