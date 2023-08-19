3ff87e64df82cd51918f66b3effbc634763495b8
LesediMoko <lesedimokonyama@gmail.com>
2023-08-19 16:27:29 +0200

    create-friendship dto swaggerui updated

--- Capstone MeetApp/libs/api/backend/src/friendships/dto/create-friendship.dto.ts
+++ Capstone MeetApp/libs/api/backend/src/friendships/dto/create-friendship.dto.ts
@@ -2,13 +2,13 @@ import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateFriendshipDto{
 
-    @ApiProperty({example: "659223dedd65fe64879e13dc", description : "The ID of the user who sent the request as in the database.", type: "string"})
+    @ApiProperty({example: "659223dedd65fe64879e13dc", description : "The ID of the user who sent the request as in the database.", type: "string", required: true})
     readonly requester!:string;
 
-    @ApiProperty({example: "64722495cd65fc66879f3ddd", description : "The ID of the user who received the request as in the database.", type: "string"})
+    @ApiProperty({example: "64722495cd65fc66879f3ddd", description : "The ID of the user who received the request as in the database.", type: "string", required: true})
     readonly requestee!:string;
 
-    @ApiProperty({example: "false", description : "Whether the friendship request was accepted or not.", type: "boolean"})
+    @ApiProperty({example: "false", description : "Whether the friendship request was accepted or not.", type: "boolean", required: true})
     readonly status!:string;
     
 }
