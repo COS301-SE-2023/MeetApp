665a482783c027a487ffe2b679a0e3aa1149ea5e
LesediMoko <lesedimokonyama@gmail.com>
2023-08-19 16:28:00 +0200

    user schema swaggerui updated

--- Capstone MeetApp/libs/api/backend/src/users/schema/user.schema.ts
+++ Capstone MeetApp/libs/api/backend/src/users/schema/user.schema.ts
@@ -10,23 +10,23 @@ export type UserDocument = HydratedDocument<User>;
 @Schema({collection : "Users"})
 export class User {
 
-  @ApiProperty({example: "747223dedd65fc64879e13dc", description : "The user's ID as in the database.", type: "string"})
+  @ApiProperty({example: "747223dedd65fc64879e13dc", description : "The user's ID as in the database.", type: "string", required: true})
   @Prop()
   ID!: mongoose.Schema.Types.ObjectId;
 
-  @ApiProperty({example: "user_man23", description : "The user's desired username."})
+  @ApiProperty({example: "user_man23", description : "The user's desired username.", required: true})
   @Prop()
   username!: string;
 
-  @ApiProperty({example: "password101", description : "The user's chosen password."})
+  @ApiProperty({example: "password101", description : "The user's chosen password.", required: true})
   @Prop()
   password!: string;
 
-  @ApiProperty({example: "em=e?vftb+asi...", description : "Base64/BSON representation of profile picture."})
+  @ApiProperty({example: "em=e?vftb+asi...", description : "Base64/BSON representation of profile picture.", required: true})
   @Prop()
   profilePicture!: string;
 
-  @ApiProperty({example: "Joburg", description : "The user's current region."})
+  @ApiProperty({example: "Joburg", description : "The user's current region.", required: true})
   @Prop()
   region!: string;
   
