914afd909f8f1fbe703f70b9f136bd6de4a57996
LesediMoko <lesedimokonyama@gmail.com>
2023-08-19 16:13:34 +0200

    create-friendship dto swaggerui deco

--- Capstone MeetApp/libs/api/backend/src/friendships/dto/create-friendship.dto.ts
+++ Capstone MeetApp/libs/api/backend/src/friendships/dto/create-friendship.dto.ts
@@ -5,7 +5,7 @@ export class CreateFriendshipDto{
     @ApiProperty({example: "659223dedd65fe64879e13dc", description : "The ID of the user who sent the request as in the database.", type: "string"})
     readonly requester!:string;
 
-    @ApiProperty({example: "619e73defd65ce648e9e63fc", description : "The ID of the user who received the request as in the database.", type: "string"})
+    @ApiProperty({example: "64722495cd65fc66879f3ddd", description : "The ID of the user who received the request as in the database.", type: "string"})
     readonly requestee!:string;
 
     @ApiProperty({example: "false", description : "Whether the friendship request was accepted or not.", type: "boolean"})
