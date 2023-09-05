851412cad8ff31f17f1e3aa84dca9290ed03e0b5
LesediMoko <lesedimokonyama@gmail.com>
2023-09-05 13:29:00 +0200

    inclusion of jwt private key env variable

--- Capstone MeetApp/libs/api/backend/src/organisations/organisations.module.ts
+++ Capstone MeetApp/libs/api/backend/src/organisations/organisations.module.ts
@@ -6,7 +6,6 @@ import { EventSchema } from '../events/schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { EventsModule } from '../events/events.module';
 import { Organisation, OrganisationSchema } from './schema';
-import { jwtConstants } from './constant';
 import { JwtModule } from '@nestjs/jwt';
 import { Attendance, AttendanceSchema } from '../attendances/schema';
 import { User, UserSchema } from '../users/schema';
@@ -14,7 +13,7 @@ import { User, UserSchema } from '../users/schema';
 @Module({
   imports: [MongooseModule.forFeature([{ name: Organisation.name, schema: OrganisationSchema }]),EventsModule,MongooseModule.forFeature([{ name: Event.name, schema: EventSchema }, {name : Attendance.name, schema: AttendanceSchema}, {name : User.name, schema: UserSchema}]), JwtModule.register({
     global: true,
-    secret: jwtConstants.secret,
+    secret: process.env['JWT_PRIVATE_KEY'],
     signOptions: { expiresIn: '1 day' },
   })],
   controllers: [OrganisationsController],
