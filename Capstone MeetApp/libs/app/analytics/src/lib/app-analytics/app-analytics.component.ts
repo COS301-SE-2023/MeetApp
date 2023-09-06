e52b3c9e4877b8b723150292adca0ef2086efa90
shiluvelomakhubele <117033636+ShiluveloMakhubel@users.noreply.github.com>
2023-09-06 23:54:38 +0200

    analytics component

---
+++ Capstone MeetApp/libs/app/analytics/src/lib/app-analytics/app-analytics.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'capstone-meet-app-app-analytics',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app-analytics.component.html',
+  styleUrls: ['./app-analytics.component.css'],
+})
+export class AppAnalyticsComponent {}
