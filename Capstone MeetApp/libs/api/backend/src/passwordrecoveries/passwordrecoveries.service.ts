da92982af22be36a6a463f5692859075d0616c28
LesediMoko <lesedimokonyama@gmail.com>
2023-09-21 06:44:30 +0200

    logic to send recovery email

--- Capstone MeetApp/libs/api/backend/src/passwordrecoveries/passwordrecoveries.service.ts
+++ Capstone MeetApp/libs/api/backend/src/passwordrecoveries/passwordrecoveries.service.ts
@@ -3,6 +3,8 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { PasswordRecovery } from './schema';
 import { CreatePasswordRecoveryDto } from './dto/create-passwordrecovery.dto';
+import { createTransport} from 'nodemailer';
+import {hash} from 'bcrypt'
 
 @Injectable()
 export class PasswordRecoveriesService {
@@ -32,5 +34,55 @@ export class PasswordRecoveriesService {
      throw new NotFoundException(`Password recovery #${id} not found`);
    }
    return deletedPR;
-}
+  }
+
+  async sendEmail(userEmail : string){
+    const userSalt = this.getUserSalt(userEmail)
+    const hashToken = (await hash(userEmail, userSalt)).replace('/','x').replace('$','d')
+    const recoveryLink = 'https://localhost:3000/api/' + hashToken
+    const transporter = createTransport({
+      host: process.env['SMTP_HOST'],
+    port: 2525,
+    auth: {
+        user: process.env['SMTP_USER'],
+        pass: process.env['SMTP_PASS']
+      }
+    });
+    const expirationTime = Date.now() + 10 * 60 * 1000
+    const PRObject = {emailAddress: userEmail, token : hashToken, expiration : expirationTime}
+    console.log(PRObject)
+
+
+    const mailOptions = {
+      from: process.env['SMTP_SENDER'],
+      to: userEmail,
+      subject: 'MeetApp: Forgot Password',
+      text: 'Click the link below to recover your account.\n' + recoveryLink + '\n\n' +'Click the link below to unsubscribe',
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error:', error);
+      } else {
+        console.log('Email sent:', info.response);
+        this.create
+      }
+    });
+  }
+
+  getAsciiSum(str : string) {
+    let sum = 0;
+  
+    for (let i = 0; i < str.length; i++) {
+      const charCode = str.charCodeAt(i);
+      sum += charCode;
+    }
+  
+    return sum;
+  }
+
+  getUserSalt(usermail : string){
+    return (this.getAsciiSum(usermail) * usermail.length) % 5
+  }
+
 }
