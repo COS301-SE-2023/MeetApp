896ba12c224deb177c7669ab8276e1bd2b357819
LesediMoko <lesedimokonyama@gmail.com>
2023-09-22 21:06:15 +0200

    addition of user model in password recovery

--- Capstone MeetApp/libs/api/backend/src/passwordrecoveries/passwordrecoveries.module.ts
+++ Capstone MeetApp/libs/api/backend/src/passwordrecoveries/passwordrecoveries.module.ts
@@ -3,9 +3,10 @@ import { PasswordRecoveriesService } from './passwordrecoveries.service';
 import { PasswordRecoveriesController } from './passwordrecoveries.controller';
 import { PasswordRecovery, PasswordRecoverySchema } from './schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from '../users/schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: PasswordRecovery.name, schema: PasswordRecoverySchema }])],
+  imports: [MongooseModule.forFeature([{ name: PasswordRecovery.name, schema: PasswordRecoverySchema }, {name : User.name, schema: UserSchema}])],
   controllers: [PasswordRecoveriesController],
   providers: [PasswordRecoveriesService]
 })
