2a40b5707b2a66c47ec71ed5d9c40b8dd5e1cf83
LesediMoko <lesedimokonyama@gmail.com>
2023-09-25 00:34:03 +0200

    chat-related logic that use Chat collection in db

---
+++ Capstone MeetApp/libs/api/backend/src/chat/chat.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@nestjs/common';
+import { ChatGateway } from './chat.gateway';
+import { Chat } from './schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateChatDto } from './dto/create-chat.dto';
+
+@Injectable()
+export class ChatService {
+constructor(private readonly chatGateway: ChatGateway, @InjectModel(Chat.name) private chatModel: Model<Chat>) {}
+  
+  async getRoomMessages(roomId: string){
+    
+    const roomDoc = await this.chatModel.findOne({roomID : roomId}).exec()
+    if (!roomDoc) return {message: "Chat room does not exist"}
+    return roomDoc?.messages;
+  }
+
+  async getRoom(roomId: string){
+    
+    const roomDoc = await this.chatModel.findOne({roomID : roomId}).exec()
+    if (!roomDoc) return {message: "Chat room does not exist"}
+    return roomDoc;
+  }
+
+  async getCreator(roomId: string){
+    
+    const roomDoc = await this.chatModel.findOne({roomID : roomId}).exec()
+    if (!roomDoc) return {message: "Chat room does not exist"}
+    return roomDoc.organisationID;
+  }
+
+  // Example: Save chat room history
+  async saveMessages(chatRoomDetails : CreateChatDto){
+    const newChatRoom = await new this.chatModel(chatRoomDetails)
+    
+    return newChatRoom.save();
+  }
+
+  async checkBanned(userId : string, roomId : string){
+    const roomDoc = await this.chatModel.findOne({roomID : roomId}).exec()
+    if (!roomDoc) return {message: "Room does not exist"}
+    if (roomDoc.bannedUsers.find(userId => userId))
+      return true
+    else 
+      return false
+  }
+
+  async checkCreator(userId : string, roomId : string){
+    const roomDoc = await this.chatModel.findOne({roomID : roomId}).exec()
+    if (!roomDoc) return {message: "Room does not exist"}
+    if (roomDoc.organisationID.toString() == userId)
+      return true
+    else 
+      return false
+  }
+
+}
