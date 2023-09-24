246bd0cbdb8db26025e34f34cc50be26ecfa28cf
LesediMoko <lesedimokonyama@gmail.com>
2023-09-25 00:44:02 +0200

    bug fix, inclusion of chat model in chat module

--- Capstone MeetApp/libs/api/backend/src/chat/chat.module.ts
+++ Capstone MeetApp/libs/api/backend/src/chat/chat.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
+import { Chat, ChatSchema } from './schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Chat.name, schema: ChatSchema }])],
   providers: [ChatGateway, ChatService],
   controllers: [ChatController],
 })
