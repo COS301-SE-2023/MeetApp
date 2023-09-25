165d2738061082aa07e34db9f04902b4a41b3190
Akani Hlungwani <116602464+AkaniHlungwani@users.noreply.github.com>
2023-09-25 10:35:24 +0200

    try fixing test errors

--- Capstone MeetApp/libs/app/forgotpassword/src/lib/app-forgotpassword/app-forgotpassword.component.spec.ts
+++ Capstone MeetApp/libs/app/forgotpassword/src/lib/app-forgotpassword/app-forgotpassword.component.spec.ts
@@ -1,25 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppForgotpasswordComponent } from './app-forgotpassword.component';
-
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 describe('AppForgotpasswordComponent', () => {
-  //let component: AppForgotpasswordComponent;
-  //let fixture: ComponentFixture<AppForgotpasswordComponent>;
+  let component: AppForgotpasswordComponent;
+  let fixture: ComponentFixture<AppForgotpasswordComponent>;
 
-  /*
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppForgotpasswordComponent],
+      imports: [AppForgotpasswordComponent, RouterTestingModule,],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AppForgotpasswordComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-  */
-  /*
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  */
-
 });
