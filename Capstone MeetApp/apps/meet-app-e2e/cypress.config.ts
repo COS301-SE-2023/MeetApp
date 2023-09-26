0b80ae0e7f2499f57fa1284096fcadc1e5e728cd
Jacob-Mahloko <114677637+Jacob-Mahloko@users.noreply.github.com>
2023-09-26 19:24:09 +0200

    🚀

--- Capstone MeetApp/apps/meet-app-e2e/cypress.config.ts
+++ Capstone MeetApp/apps/meet-app-e2e/cypress.config.ts
@@ -3,6 +3,6 @@ import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 import '@cypress/webpack-preprocessor';
 export default defineConfig({
   e2e: nxE2EPreset(__dirname),
-  defaultCommandTimeout:7000
+  defaultCommandTimeout:9000
 });
 
