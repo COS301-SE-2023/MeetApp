8c3736e5b82a4d34afbfb4619aadcce4049c5a17
LesediMoko <lesedimokonyama@gmail.com>
2023-09-27 18:52:50 +0200

    update imports and providers

--- Capstone MeetApp/libs/api/backend/src/recommendations/recommendations.module.ts
+++ Capstone MeetApp/libs/api/backend/src/recommendations/recommendations.module.ts
@@ -3,10 +3,15 @@ import { RecommendationsService } from './recommendations.service';
 import { RecommendationsController } from './recommendations.controller';
 import { Recommendation, RecommendationSchema } from './schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersService } from '../users/users.service';
+import { User, UserSchema } from '../users/schema';
+import { Organisation, OrganisationSchema } from '../organisations/schema';
+import { Attendance, AttendanceSchema } from '../attendances/schema';
+import { Event, EventSchema } from '../events/schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Recommendation.name, schema: RecommendationSchema }])],
+  imports: [MongooseModule.forFeature([{ name: Recommendation.name, schema: RecommendationSchema }, { name: User.name, schema: UserSchema }, { name: Organisation.name, schema: OrganisationSchema }, { name: Attendance.name, schema: AttendanceSchema }, { name: Event.name, schema: EventSchema }])],
   controllers: [RecommendationsController],
-  providers: [RecommendationsService]
+  providers: [RecommendationsService, UsersService]
 })
 export class RecommendationModule {}
