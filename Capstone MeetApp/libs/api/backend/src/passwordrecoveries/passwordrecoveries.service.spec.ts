09486c7947ae2f7e90745c361f58a572e27d52f3
LesediMoko <lesedimokonyama@gmail.com>
2023-09-28 03:45:40 +0200

    test file updates

--- Capstone MeetApp/libs/api/backend/src/passwordrecoveries/passwordrecoveries.service.spec.ts
+++ Capstone MeetApp/libs/api/backend/src/passwordrecoveries/passwordrecoveries.service.spec.ts
@@ -4,6 +4,7 @@ import { PasswordRecovery } from './schema';
 import { getModelToken } from '@nestjs/mongoose';
 import { User } from '../users/schema';
 import { Organisation } from '../organisations/schema';
+import { JwtModule } from '@nestjs/jwt';
 
 describe('PasswordRecoveryService', () => {
   let service: PasswordRecoveriesService;
@@ -11,7 +12,13 @@ describe('PasswordRecoveryService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [PasswordRecoveriesService, { provide: getModelToken(PasswordRecovery.name), useValue: jest.fn() }, { provide: getModelToken(User.name), useValue: jest.fn() }, { provide: getModelToken(Organisation.name), useValue: jest.fn() }],
-    }).compile();
+      imports: [
+        JwtModule.register({
+          global: true,
+          secret: process.env['JWT_PRIVATE_KEY'],
+          signOptions: { expiresIn: '1 day' },
+        })],
+      }).compile();
 
     service = module.get<PasswordRecoveriesService>(PasswordRecoveriesService);
   });
