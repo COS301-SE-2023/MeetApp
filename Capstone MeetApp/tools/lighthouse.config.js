278c2df27f696d0b112269d22c8edde4605eddbb
Jacob-Mahloko <114677637+Jacob-Mahloko@users.noreply.github.com>
2023-09-29 01:40:11 +0200

    ✨

--- Capstone MeetApp/tools/lighthouse.config.js
+++ Capstone MeetApp/tools/lighthouse.config.js
@@ -6,8 +6,12 @@ const report = JSON.parse(fs.readFileSync('../lighthouse-report.json', 'utf8'));
 // Extract performance and availability metrics
 const performanceScore = report.categories.performance.score * 100; // Performance score as a percentage (0-100)
 const availabilityScore = report.categories.accessibility.score * 100; // Availability score as a percentage (0-100)
-
+const bestPracticesScore = report.categories['best-practices'].score * 100;
+const seoScore = report.categories.seo.score * 100;
+console.log('MEETAPP NON-FUNCTIONAL TESTING..............................')
 console.log(`Performance Score: ${performanceScore}`);
 console.log(`Availability Score: ${availabilityScore}`);
-
+console.log(`Best Practice Score:${bestPracticesScore}`);
+console.log(`SEO Score: ${seoScore}`);
+console.log(`MEETAPP TESTING ENDING...........................`);
   
