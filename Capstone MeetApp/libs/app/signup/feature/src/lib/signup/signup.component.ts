18cb5a07148296ecb925e7a77f506aa46f557200
Jacob-Mahloko <114677637+Jacob-Mahloko@users.noreply.github.com>
2023-10-05 21:25:08 +0200

    ⚡️ added password

--- Capstone MeetApp/libs/app/signup/feature/src/lib/signup/signup.component.ts
+++ Capstone MeetApp/libs/app/signup/feature/src/lib/signup/signup.component.ts
@@ -29,7 +29,8 @@ export class SignupComponent {
   
   loginForm!: FormGroup;
   valid=true;
-
+  emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
   constructor(private router: Router, private formBuilder: FormBuilder, private apiService: service,private alertController: AlertController,
     private toastController: ToastController,private activatedRoute: ActivatedRoute,private location: Location, public loadingController: LoadingController) {}
     selectedOptions: string[] = [];
@@ -107,10 +108,10 @@ export class SignupComponent {
         username: ['', Validators.required],
         region:['', Validators.required],
         selectedOptions:['', Validators.required],
-        password: ['', [Validators.required, Validators.minLength(8)]],
+        password: ['', [Validators.required, Validators.minLength(8),Validators.pattern(this.passwordPattern)]],
         confirmpassword: ['', Validators.required],
         name:['', Validators.required],
-        email:['',Validators.required]
+        email:['',Validators.pattern(this.emailPattern)]
         });
         
 
@@ -200,12 +201,17 @@ export class SignupComponent {
           {
             this.valid_passregex=false;
           }
+          if(this.checkEmail(email)==true){
+            this.valid_email=true;
+          }else{
+            this.valid_email=false;
+          }
 
           console.log('Password is equal to CP',this.valid_pass);
           console.log('Regex',this.valid_passregex);
           console.log('Valid User name ',this.valid_user);
 
-          if(this.valid_pass && this.valid_user && this.valid_passregex)
+          if(this.valid_pass && this.valid_user && this.valid_passregex && this.valid_email)
           {
             console.log(region);
             console.log(this.selectedOptions);
@@ -253,16 +259,16 @@ export class SignupComponent {
             this.valid_passregex=false;
           }
 
-          if(this.checkEmail(email)==true){
-            this.valid_email=true;
-          }else{
-            this.valid_email=false;
-          }
+          // if(this.checkEmail(email)==true){
+          //   this.valid_email=true;
+          // }else{
+          //   this.valid_email=false;
+          // }
           console.log('Password is equal to CP',this.valid_pass);
           console.log('Regex',this.valid_passregex);
           console.log('Valid User name ',this.valid_user);
           console.log('valid email',this.valid_email);
-          if(this.valid_pass && this.valid_user && this.valid_passregex &&this.valid_email)
+          if(this.valid_pass && this.valid_user && this.valid_passregex )
           {
           
             this.SignUpOrg(email,username,name,password, this.events)
