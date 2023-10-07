5d2e7d4974c74d78d837db29c81b83ee7f765c81
LesediMoko <lesedimokonyama@gmail.com>
2023-10-07 21:32:38 +0200

    pending accounts endpoints fixed

--- Capstone MeetApp/libs/api/backend/src/passwordrecoveries/passwordrecoveries.controller.ts
+++ Capstone MeetApp/libs/api/backend/src/passwordrecoveries/passwordrecoveries.controller.ts
@@ -44,16 +44,22 @@ export class PasswordRecoveriesController {
   }
 
   @Post('send')
+  @ApiOperation({ summary: 'Send the recovery email' }) 
+  @ApiResponse({ status: 201, description: 'A message and payload' })
   async emailSend(@Body('emailAddress') email: string){
     return await this.passwordRecoveryService.sendEmail(email)
   }
 
   @Get('verify')
+  @ApiOperation({ summary: 'verify the recovery token' }) 
+  @ApiResponse({ status: 201, description: 'A message and payload' })
   async tokenVerify(@Query('token') token: string, @Query('email') email: string){
     return await this.passwordRecoveryService.verifyEmailToken(email, token)
   }
 
   @Patch('recover')
+  @ApiOperation({ summary: 'Recover your account' }) 
+  @ApiResponse({ status: 201, description: 'A message and payload' })
   async recoverPassword(@Body('email') email: string, @Body('password') password: string){
     return await this.passwordRecoveryService.passwordrest(email, password)
   }
