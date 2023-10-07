193b3fade40a7defd9200d3c8c5d6d0ffc0ebffa
LesediMoko <lesedimokonyama@gmail.com>
2023-10-07 21:32:52 +0200

    pendingaccounts test files updated

--- Capstone MeetApp/libs/api/backend/src/pendingaccounts/pendingaccounts.controller.spec.ts
+++ Capstone MeetApp/libs/api/backend/src/pendingaccounts/pendingaccounts.controller.spec.ts
@@ -3,6 +3,11 @@ import { PendingAccountsController } from './pendingaccounts.controller';
 import { PendingAccountsService } from './pendingaccounts.service';
 import { getModelToken } from '@nestjs/mongoose';
 import { PendingAccount } from './schema';
+import { Friendship } from '../friendships/schema';
+import { Organisation } from '../organisations/schema';
+import { User } from '../users/schema';
+import { Attendance } from '../attendances/schema';
+import { Event } from '../events/schema';
 
 describe('PendingAccountsController', () => {
   let controller: PendingAccountsController;
@@ -10,7 +15,7 @@ describe('PendingAccountsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PendingAccountsController],
-      providers: [PendingAccountsService, { provide: getModelToken(PendingAccount.name), useValue: jest.fn() }],
+      providers: [PendingAccountsService, { provide: getModelToken(PendingAccount.name), useValue: jest.fn() }, { provide: getModelToken(User.name), useValue: jest.fn() }, { provide: getModelToken(Attendance.name), useValue: jest.fn() }, { provide: getModelToken(Event.name), useValue: jest.fn() }, { provide: getModelToken(Organisation.name), useValue: jest.fn() }, { provide: getModelToken(Friendship.name), useValue:  jest.fn()}],
     }).compile();
 
     controller = module.get<PendingAccountsController>(PendingAccountsController);
--- Capstone MeetApp/libs/api/backend/src/pendingaccounts/pendingaccounts.service.spec.ts
+++ Capstone MeetApp/libs/api/backend/src/pendingaccounts/pendingaccounts.service.spec.ts
@@ -2,13 +2,18 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PendingAccountsService } from './pendingaccounts.service';
 import { PendingAccount } from './schema';
 import { getModelToken } from '@nestjs/mongoose';
+import { Organisation } from '../organisations/schema';
+import { Friendship } from '../friendships/schema';
+import { User } from '../users/schema';
+import { Attendance } from '../attendances/schema';
+import { Event } from '../events/schema';
 
 describe('PendingAccountsService', () => {
   let service: PendingAccountsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PendingAccountsService, { provide: getModelToken(PendingAccount.name), useValue: jest.fn() }],
+      providers: [PendingAccountsService, { provide: getModelToken(PendingAccount.name), useValue: jest.fn() }, { provide: getModelToken(User.name), useValue: jest.fn() }, { provide: getModelToken(Attendance.name), useValue: jest.fn() }, { provide: getModelToken(Event.name), useValue: jest.fn() }, { provide: getModelToken(Organisation.name), useValue: jest.fn() }, { provide: getModelToken(Friendship.name), useValue:  jest.fn()}],
     }).compile();
 
     service = module.get<PendingAccountsService>(PendingAccountsService);
